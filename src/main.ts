4ae697d8b84093824b96833e08e25d8518891ed1
Michael Robertson <mrobertson@nationalmerchants.org>
2020-05-10 11:56:24 -0700

    add cors handling

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ dotenv.config();
 import "reflect-metadata";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const appPort = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   await app.listen(appPort);
   console.log(`Listening on ${appPort}`);
 }
