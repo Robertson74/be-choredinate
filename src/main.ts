5d8608be55d2efdb05e9c946fcf1b2e640f44740
Michael Robertson <mrobertson@nationalmerchants.org>
2020-05-10 10:40:51 -0700

    get app to working state

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const appPort = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(appPort);
+  console.log(`Listening on ${appPort}`);
 }
 bootstrap();
